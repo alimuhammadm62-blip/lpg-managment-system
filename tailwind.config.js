fe40ccdadfc0118c0f3cfccd4079b6d976545678
Muhammad Ali <alimuhammadm62@gmail.com>
2025-11-27 06:56:52 +0500

    Updated system

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // add other folders/files you use JSX/TSX in
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
